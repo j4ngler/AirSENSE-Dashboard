611219d6a91cd3595f846e49c0d75e2e0d2a31bd
j4ngler <baokute4@gmail.com>
2022-11-26 22:24:39 +0700

    update data senor page

--- react_customer/src/configs/routes.js
+++ react_customer/src/configs/routes.js
@@ -4,6 +4,7 @@ import NotFoundPage from "../pages/Error/NotFound";
 import LoginPage from "../pages/Auth/login";
 import Dashboard from "../pages/Dashboard";
 import Account from "../pages/Account";
+import DataSensor from "../pages/DataSensors"
 
 const routes = [
   {
@@ -14,6 +15,8 @@ const routes = [
       { path: "account", element: <Account /> },
       { path: "404", element: <NotFoundPage /> },
       { path: "*", element: <Navigate to="/customer/404" /> },
+      { path: "station/data_station", element: <DataSensor /> },
+      
     ],
   },
   {
