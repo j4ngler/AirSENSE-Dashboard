4bb0633e4fd59111d144cbfa020879cd88aaa893
j4ngler <baokute4@gmail.com>
2022-12-01 22:23:38 +0700

    update map

---
+++ react_customer/src/components/Map/leafletmap.js
@@ -0,0 +1,36 @@
+import React from 'react'
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import markerIconPng from "leaflet/dist/images/marker-icon.png"
+import L from 'leaflet'
+import 'leaflet/dist/leaflet.css';
+import './Map.css';
+
+let DefaultIcon = L.icon({
+    iconUrl: markerIconPng,
+});
+L.Marker.prototype.options.icon = DefaultIcon;
+
+const LeafletMap = () => {
+    const position = [21.0057398, 105.8424833];
+
+    return (
+        <div>
+            <MapContainer center={position} zoom={20} scrollWheelZoom={false}>
+                <TileLayer
+                    attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                    url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
+                />
+                <Marker position={position}>
+                    <Popup>
+                    A pretty CSS3 popup. <br /> Easily customizable.
+                    </Popup>
+                </Marker>
+            </MapContainer>
+        </div>
+    )
+}
+
+export default LeafletMap;
+
+
+
--- react_customer/src/configs/routes.js
+++ react_customer/src/configs/routes.js
@@ -5,6 +5,8 @@ import LoginPage from "../pages/Auth/login";
 import Dashboard from "../pages/Dashboard";
 import Account from "../pages/Account";
 import DataSensor from "../pages/DataSensors"
+import GGMap from "../components/Map/ggmap";
+import LeafletMap from "../components/Map/leafletmap";
 
 const routes = [
   {
@@ -16,6 +18,7 @@ const routes = [
       { path: "404", element: <NotFoundPage /> },
       { path: "*", element: <Navigate to="/customer/404" /> },
       { path: "station/data_station", element: <DataSensor /> },
+      { path: "station/list_station", element: <LeafletMap />}
     ],
   },
   {
