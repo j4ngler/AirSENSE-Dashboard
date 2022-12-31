0daa80bf15098c6a249dfea0e833c3b7854e6e5b
j4ngler <baokute4@gmail.com>
2022-12-31 20:23:50 +0700

    list comment

--- react_comment/src/component/Input/index.js
+++ react_comment/src/component/Input/index.js
@@ -321,8 +321,91 @@ const { MentionSuggestions, plugins } = useMemo(() => {
        
        </div> */}
         
-        </div>     
-        </>
+        </div>  
+        <div>
+          <div className='update'>
+            <button className='popular'>Popular</button>
+            <button className='newest'>Newest</button>
+          </div>
+          <div className='box-inf-comment'>
+            <div className='comment'>
+              <div className=''>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 img-user-comment">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+              </div>
+              <div className='inf-comment'>
+                <h4 className='username'>Olivia Gribben</h4>
+                <div className='describe-comment'>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, accusantium consequatur. Perspiciatis!</p>
+                  <p>
+                    Lorem ipsum dolor sit, amet
+                    <span>
+                      ...
+                      <button className='read-more'>Read more</button>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='emotion-comment'>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 heart">
+                      <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                    </svg>
+                  </span>
+                  <span>5</span>
+                  <span className='dots'>.</span>
+                  <span><a className='reply'>Reply</a></span>
+                  <span className='dots'>.</span>
+                  <span className=''>1h</span>
+                  
+            </div>
+          </div>
+          <div className='box-inf-comment'>
+            <div className='comment'>
+              <div className=''>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 img-user-comment">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z" />
+                </svg>
+              </div>
+              <div className='inf-comment'>
+                <h4 className='username'>Olivia Gribben</h4>
+                <div className='describe-comment'>
+                  <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum, accusantium consequatur. Perspiciatis!</p>
+                  <p>
+                    Lorem ipsum dolor sit, amet
+                    <span>
+                      ...
+                      <button className='read-more'>Read more</button>
+                    </span>
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className='emotion-comment'>
+                  <span>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 heart">
+                      <path d="M11.645 20.91l-.007-.003-.022-.012a15.247 15.247 0 01-.383-.218 25.18 25.18 0 01-4.244-3.17C4.688 15.36 2.25 12.174 2.25 8.25 2.25 5.322 4.714 3 7.688 3A5.5 5.5 0 0112 5.052 5.5 5.5 0 0116.313 3c2.973 0 5.437 2.322 5.437 5.25 0 3.925-2.438 7.111-4.739 9.256a25.175 25.175 0 01-4.244 3.17 15.247 15.247 0 01-.383.219l-.022.012-.007.004-.003.001a.752.752 0 01-.704 0l-.003-.001z" />
+                    </svg>
+                  </span>
+                  <span>5</span>
+                  <span className='dots'>.</span>
+                  <span><a className='reply'>Reply</a></span>
+                  <span className='dots'>.</span>
+                  <span className=''>1h</span>
+            </div>
+            <br></br>
+            <button className='view-more'>
+              <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="w-6 h-6 margin-top-2px">
+                <path fillRule="evenodd" d="M4.5 5.653c0-1.426 1.529-2.33 2.779-1.643l11.54 6.348c1.295.712 1.295 2.573 0 3.285L7.28 19.991c-1.25.687-2.779-.217-2.779-1.643V5.653z" clipRule="evenodd" />
+              </svg>
+              <span className='padding-l-r-8px'>View 2 replies</span>
+            </button>
+          </div>    
+        </div> 
+          
+    </>
     )
 }
 
