3374c45cbf27beba74e0c117806d6fd8862eaf6b
j4ngler <baokute4@gmail.com>
2023-07-15 10:50:16 +0700

    dashboard

--- WebManage/server/controllers/customer.controller.js
+++ WebManage/server/controllers/customer.controller.js
@@ -790,12 +790,12 @@ customerCtrl.getDataAverage = async (req, res) => {
       .select()
       .from("data_average")
       .order("time", false)
-      .limit(1)
+      .limit(3)
       .toString();
       //query vao database
     const data = await knex.raw(sqlString)
    //gui ve cho nguoi dung
-    res.status(200).json(data[0][0]);
+    res.status(200).json(data[0]);
   } catch (error) {
     res.status(500).json({
       message:error
